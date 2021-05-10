76b1640e3613732b384e20d4f044e26ef8f5b970
Travis Zhang <traviszhang2002@gmail.com>
2021-05-09 21:41:21 -0400

    Updated init env file

--- gym_env/__init__.py
+++ gym_env/__init__.py
@@ -9,3 +9,9 @@ register(id='neuron_poker-v1',
     
 register(id='neuron_poker-v2',
          entry_point='gym_env.env_v2:HoldemTable')
+
+register(id='neuron_poker-v7',
+         entry_point='gym_env.env_v7:HoldemTable')
+
+register(id='neuron_poker-v8',
+         entry_point='gym_env.env_v8:HoldemTable')
